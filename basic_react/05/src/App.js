afb981e68c24d4cedb807c0e7c843f98320c2b95
tianci <newanobj@foxmail.com>
2022-08-10 12:09:39 +0800

    项目搭建配置路由完成

--- basic_react/05/src/App.js
+++ basic_react/05/src/App.js
@@ -1,4 +1,19 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Layout'
 function App() {
-  return <div className="App">App根组件</div>
+  return (
+    <Router>
+      <div className="App">
+        {/* <h3>App根组件</h3> */}
+        {/* <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link> */}
+      </div>
+      <Switch>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
+      </Switch>
+    </Router>
+  )
 }
 export default App
