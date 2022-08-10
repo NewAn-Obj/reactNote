c2ededa63b876a3d369805ab01a2d597034d9659
tianci <newanobj@foxmail.com>
2022-08-10 11:06:29 +0800

    初始化05写极客园PC，并搭建项目基础结构

---
+++ basic_react/05/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+// 渲染组件
+ReactDOM.createRoot(document.querySelector('#root')).render(<App />)
