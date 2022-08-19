c972e4a8c17df5afbbe835f697cef322828edd5a
tianci <newanobj@foxmail.com>
2022-08-19 14:46:00 +0800

    配置开发者工具redux-devtools-extension

--- basic_react/07_redux/redux_in_react/src/store/index.js
+++ basic_react/07_redux/redux_in_react/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 /**
  * 1. 安装 yarn add react-thunk
  * 2.引入thunk
@@ -7,6 +8,6 @@ import reducer from './reducer'
  */
 import thunk from 'redux-thunk'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
